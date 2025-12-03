63e02c4aba7f1c9ddf7edf783c4072d86101e3dc
NiklasRoos91 <niklas_roos@live.se>
2025-12-03 13:59:42 +0100

    implemented GetFlashcardListWithFlashcards endpoint and run tests

---
+++ Flashcards.Application/Features/FlashcardlistsFeature/Queries/GetFlashcardListWithFlashcards/GetFlashcardListWithFlashcardsQuery.cs
@@ -0,0 +1,18 @@
+﻿using Flashcards.Application.Commons.OperationResult;
+using Flashcards.Application.Features.FlashcardsFeature.DTOs.Responses;
+using MediatR;
+
+namespace Flashcards.Application.Features.FlashcardListsFeature.Queries.GetFlashcardListWithFlashcards
+{
+    public class GetFlashcardListWithFlashcardsQuery : IRequest<OperationResult<List<FlashcardResponseDto>>>
+    {
+        public Guid FlashcardListId { get; }
+        public Guid UserId { get; }
+
+        public GetFlashcardListWithFlashcardsQuery(Guid flashcardListId, Guid userId)
+        {
+            FlashcardListId = flashcardListId;
+            UserId = userId;
+        }
+    }
+}
